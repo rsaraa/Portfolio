9e88411292826309be1c4e3c75128ea33f8f6f61
SARAA RAMAKRISHNAN <93540467+rsaraa@users.noreply.github.com>
2024-07-04 13:33:05 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/HomeLogo.jpg";
 import Particle from "../Particle";
 
 function Home() {
